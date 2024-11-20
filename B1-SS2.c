b52c260dcd1d638c8fd9d9f5b8e37ba876677b6b
anhtuan4906 <anhtuan144906@gmail.com>
2024-11-21 00:13:08 +0700

    Add files via upload

---
+++ B1-SS2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+// kieu int: Dung de luu tru so nguyen(ca am va duong)
+int a=10;
+// kieu float: Dung de luu tru so thuc voi do chinh xac thap
+float b=3.14;
+// kieu double: Dung de luu tru so thuc voi do chinh xac cao hon float
+double c=2.481748764411;
+// kieu char: Dung de luu tru 1 ky tu
+char kyTu = 'A';
+    return 0;
+}   
