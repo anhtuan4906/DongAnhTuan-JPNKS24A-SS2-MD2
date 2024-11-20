e7df9477abb0fe7eb097ae23eb54359a161d98b6
anhtuan4906 <anhtuan144906@gmail.com>
2024-11-21 00:14:27 +0700

    Add files via upload

---
+++ B4-SS2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int canhHinhVuong = 10;
+// tinh chu vi hinh vuong
+int chuVi = 4 * canhHinhVuong;
+// tinh dien tich hinh vuong
+int dienTich = canhHinhVuong * canhHinhVuong;
+// in ket qua
+printf("Chu vi của hình vuông là %d \n",chuVi);
+printf("Dien tich của hình vuông là %d \n",dienTich);
+
+    return 0;
+
+}
