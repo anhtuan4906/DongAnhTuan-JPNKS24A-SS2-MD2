9b439c7ee3c6d78a43d11357cc07cd5ddf79fd74
anhtuan4906 <anhtuan144906@gmail.com>
2024-11-21 00:14:12 +0700

    Add files via upload

---
+++ B3-SS2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+     int so1=50;
+     int so2=5;
+     // tong cua 2 so ban dau
+     int tong = so1 + so2 ;
+     printf("Tong cua 2 so la %d",tong);
+     // hieu cua 2 so ban dau
+     int hieu = so1 - so2 ;
+     printf("\n Hieu cua 2 so la %d",hieu);
+     //tich cua 2 so ban dau
+     int tich = so1 * so2 ;
+     printf("\n Tich cua 2 so la %d",tich);
+     //thuong cua 2 so ban dau
+     int thuong = so1 / so2 ;
+     printf("\n Thuong cua 2 so la %d",thuong);
+
+    return 0;
+}
