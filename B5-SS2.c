dfa975fd2f02442d65cfc78b6573795e7f44b4f1
anhtuan4906 <anhtuan144906@gmail.com>
2024-11-21 00:14:42 +0700

    Add files via upload

---
+++ B5-SS2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+// gan chieu dai, chieu rong hinh chu nhat
+int chieuDai = 10;
+int chieuRong = 5;
+// tinh chu vi hinh chu nhat
+int chuVi = 2*( chieuDai+chieuRong);
+int dienTich = chieuDai * chieuRong;
+// In ra man hinh
+printf("Chu vi cua hinh chu nhat la %d",chuVi);
+printf("\nDien tich cua hinh chu nhat la %d", dienTich);
+
+    return 0;
+
+}
